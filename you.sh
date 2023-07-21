a1877a90513fe853e833ed71928cad74ba1352ed
tpxbot <133985363+Ythirt@users.noreply.github.com>
2023-07-21 07:45:06 +0300

    Update you.sh

--- you.sh
+++ you.sh
@@ -1,17 +1,28 @@
-#!/bin/bash
+; تحديد التطبيق المستهدف (Google Chrome)
+targetAppTitle := "Google Chrome"
 
-# رابط الفيديو من اليوتيوب الذي ترغب في فتحه
-youtube_video_url="https://www.youtube.com/watch?v=nYgSZvKWucA"
+; رابط الفيديو من اليوتيوب الذي ترغب في فتحه
+youtube_video_url := "https://www.youtube.com/watch?v=nYgSZvKWucA"
 
-# عدد النوافذ التي ترغب في فتحها وتشغيل الفيديو في كل نافذة
-num_windows=5
+; عدد النوافذ التي ترغب في فتحها وتشغيل الفيديو في كل نافذة
+num_windows := 5
 
-# حلقة تكرار لفتح النوافذ وتشغيل الفيديو في كل نافذة بشكل خفي
-for ((i = 1; i <= num_windows; i++))
-do
-  # افتح رابط الفيديو في المتصفح Google Chrome بوضع التصفح الخفي
-  "/path/to/chrome.exe" --headless --disable-gpu --incognito --new-window "$youtube_video_url" &
+; حلقة تكرار لفتح النوافذ وتشغيل الفيديو في كل نافذة بشكل خفي
+Loop % num_windows
+{
+  ; فتح رابط الفيديو في المتصفح Google Chrome بوضع التصفح الخفي
+  Run % "chrome.exe --incognito " youtube_video_url
 
-  # انتظر ثانية واحدة بين فتح كل نافذة لتجنب تداخلها
-  sleep 1
-done
+  ; انتظر قليلاً لبدء تشغيل الفيديو
+  Sleep 5000
+
+  ; تحديد النافذة المفتوحة باستخدام عنوانها
+  WinGet, hWnd, ID, % targetAppTitle
+
+  ; إرسال مفتاح المسافة (Space) لبدء تشغيل الفيديو
+  ControlSend,, {Space}, ahk_id %hWnd%
+
+  ; انتظر قليلاً ثم إغلاق النافذة
+  Sleep 10000
+  WinClose, % "ahk_id " hWnd
+}
