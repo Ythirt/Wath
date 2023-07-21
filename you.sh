271ff06f35f45b3925ab87535212cbbb47a7edcd
tpxbot <133985363+Ythirt@users.noreply.github.com>
2023-07-21 06:59:44 +0300

    Update you.sh

--- you.sh
+++ you.sh
@@ -6,11 +6,11 @@ youtube_video_url="https://www.youtube.com/watch?v=nYgSZvKWucA"
 # عدد النوافذ التي ترغب في فتحها وتشغيل الفيديو في كل نافذة
 num_windows=2000
 
-# حلقة تكرار لفتح النوافذ وتشغيل الفيديو في كل نافذة
+# حلقة تكرار لفتح النوافذ وتشغيل الفيديو في كل نافذة بشكل خفي
 for ((i = 1; i <= num_windows; i++))
 do
-  # افتح رابط الفيديو في المتصفح الافتراضي
-  xdg-open "$youtube_video_url" &
+  # افتح رابط الفيديو في المتصفح Google Chrome بوضع التصفح الخفي
+  start chrome --headless --disable-gpu --incognito "$youtube_video_url" &
 
   # انتظر ثانية واحدة بين فتح كل نافذة لتجنب تداخلها
   sleep 1
